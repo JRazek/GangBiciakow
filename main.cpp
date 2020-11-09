a87c721d5dc8739717b90d1916ee05eb1c719b8a
Jakub <jakub.jakubecky123@gmail.com>
2020-11-09 17:39:23 +0100

    streetOrder. to test!

--- main.cpp
+++ main.cpp
@@ -8,7 +8,56 @@
 //knowledge source: https://www.youtube.com/watch?v=iOz1UJPdcTQ
 using namespace std;
 struct Leaf;
-struct HeavyPath;
+struct SparseTableMin{
+    vector<int> values;
+    vector<vector<int>*> sparseTable;
+    SparseTableMin(vector<int> values){
+        this->values = values;
+        int columns = values.size();
+        int rows = log2(values.size());
+        for(int i = 0; i < rows; i ++){
+            sparseTable.push_back(new vector<int>());
+            int segmentSize = pow(2, i);
+            for(int j = 0; j < columns - segmentSize + 1; j ++){
+                if(i == 0){
+                    sparseTable[0]->push_back(values[j]);
+                } else{
+                    sparseTable[i]->push_back(getMinimum(j, j + segmentSize - 1));
+                }
+            }
+        }
+    }
+    int getMinimum(int from, int to){
+        int length = to - from + 1;
+        int rowNum = (int)(log2(length)) - 1;
+        int fixSize = pow(2, rowNum);
+        vector<int> * row = sparseTable[rowNum];
+        int firstMin = row->at(from);
+        int secondMin = row->at(to - fixSize + 1);
+        int min = firstMin > secondMin ? secondMin : firstMin;
+        return min;
+    }
+    ~SparseTableMin(){
+        for(auto v : sparseTable){
+            delete v;
+        }
+    }
+};
+
+struct Range{
+    int min;
+    int max;
+    static vector<Range *> toDelete;
+    Range(int min, int max){
+        this->min = min;
+        this->max = max;
+    }
+    static Range * commonPart(Range * r1, Range * r2){
+        int min = r1->min > r2->min ? r1->min : r2->min;
+        int max = r1->max < r2->max ? r1->max : r2->max;
+        return new Range(min, max);
+    }
+};
 struct Connection{
     Leaf * l1;
     Leaf * l2;
@@ -36,9 +85,10 @@ struct Leaf{
     vector<Connection *> connections;
     int id;
     int levelInTree;
+    int eulerTourID;
 
-    HeavyPath * heavyPath;
-    int idInHeavyPath = -1; // if our node is in heavy path - this will be the id of it counting from the top(lowest number floors)
+    int firstOccurrenceInEuler = -1;
+    int lastOccurrenceInEuler;
 
     int subTreeSize = 0;
 
@@ -46,61 +96,110 @@ struct Leaf{
         this->parentPath = parentPath;
     }
 };
-unordered_map<int,int> merge(unordered_map<int,int> m1, unordered_map<int,int> m2){
-    unordered_map<int, int> smaller = m1.size() < m2.size() ? move(m1) : move(m2);
-    unordered_map<int, int> bigger = m1.size() < m2.size() ? move(m2) : move(m1);
-    unordered_map<int,int> result = bigger;
 
-    for(unordered_map<int, int>::iterator it = smaller.begin(); it != smaller.end(); ++it){
-        if(result.find(it->first) == result.end()){
-            result[it->first] = it->second;
-        }else{
-            result[it->first] += it->second;
+void eulerTourIndexing(Leaf * node, int &index, vector<Leaf *> &tourTownOrder,  int level = 0){
+    node->eulerTourID = index;
+    node->levelInTree = level;
+    index += 1;
+    tourTownOrder.push_back(node);
+    for(int i = 0; i < node->connections.size(); i ++){
+        if(node->connections.at(i) != node->parentPath){
+            Connection * c = node->connections.at(i);
+            eulerTourIndexing(c->child, index, tourTownOrder, ++level);
+            tourTownOrder.push_back(node);
         }
     }
-    return result;
+
 }
-pair<int,int> commonPart(pair<int,int> r1, pair<int,int> r2){
-    int min = r1.first > r2.first ? r1.first : r2.first;
-    int max = r1.second < r2.second ? r1.second : r2.second;
-    return pair<int,int>(min, max);
-}
-struct HeavyPath{
-    vector<Leaf *> path;
-    /**
-     * first vector stands for floor num(counting from the bottom), second for nodes in each floor.
-     * the map is the memoization on each segment of the toys
-     */
+
+struct SegmentTree{
     struct BinaryNode{
-        BinaryNode * parent;
+        int id;
+        Range * range;
 
-        BinaryNode * left;
-        BinaryNode * right;
+        unordered_map<int,int> values;
 
-        pair<int,int> range;
-
-        unordered_map<int,int> value;
-        BinaryNode(BinaryNode * l, BinaryNode * r){
-            this->left = l;
-            this->right = r;
-            if(l != nullptr && r != nullptr){
-                this->range = pair<int,int>(l->range.first, r->range.second);
-            }
+        BinaryNode(int id, Range * r){
+            this->id = id;
+            this->range = r;
         }
-        BinaryNode(int range){
-            this->range = pair<int,int>(range, range);
+        ~BinaryNode(){
+            delete range;
         }
     };
-    vector< vector< BinaryNode*> > floors;
-    BinaryNode * root;
-    void preprocessSegmentTree(){
+    vector< BinaryNode * > nodes;
+    vector<Leaf *> eulerTour;
 
+    int firstFloorSize;
+    int height;
+    SegmentTree(vector<Leaf *> eulerTour){
+        this->eulerTour = eulerTour;//if not enough space jus add &
+        float logVal = log2(eulerTour.size());
+        this->firstFloorSize = pow(2, (int)logVal + (bool)((int)logVal != logVal));
+        this->height = log2(firstFloorSize) + 1;
+        buildTree();
     }
-    void updateValue(Leaf * node, int newToy){
 
+    void buildTree(){
+        int nodesCount = pow(2, height) - 1;
+        for(int i = 0; i < nodesCount; i ++){
+            nodes.push_back(new BinaryNode(i, new Range(0,0)));
+        }
+        for(int i = 0 ; i < firstFloorSize; i ++){
+            BinaryNode * n = nodes[i +  pow(2, height - 1) - 1];
+            n->range->min = i;
+            n->range->max = i;
+        }
+        for(int i = nodesCount - 1 - firstFloorSize; i >= 0; i --){
+            BinaryNode * n = nodes[i];
+            n->range->min = getChild(i, true)->range->min;
+            n->range->max = getChild(i, false)->range->max;
+        }
     }
 
+    BinaryNode * getChild(int binaryNodeID, bool left){
+        return nodes[(binaryNodeID) * 2 + !left + 1];
+    }
+    BinaryNode * getParent(int binaryNodeID){
+        return nodes[(binaryNodeID - 1) / 2];
+    }
+    vector<BinaryNode *> rangeQuery(Range * range, int nodeID = 0){
+        if(range == nullptr){
+            return vector<BinaryNode *>();
+        }
+        BinaryNode * parent = nodes[nodeID];
+
+        if(range->min == parent->range->min && range->max == parent->range->max){
+            return {parent};
+        }
+
+        BinaryNode * leftChild = getChild(nodeID, true);
+        BinaryNode * rightChild = getChild(nodeID, false);
+
+
+
+        Range * commonLeft = Range::commonPart(range, leftChild->range);
+        Range * commonRight = Range::commonPart(range, rightChild->range);
+        vector<BinaryNode *> result;
+        if(commonLeft->min <= commonLeft->max){
+            vector<BinaryNode *> resLeft = rangeQuery(commonLeft, leftChild->id);
+            result.insert(result.begin(), resLeft.begin(), resLeft.end());
+        }
+        if(commonRight->min <= commonRight->max){
+            vector<BinaryNode *> resRight = rangeQuery(commonRight, rightChild->id);
+            result.insert(result.begin(), resRight.begin(), resRight.end());
+        }
+        delete commonRight;
+        delete commonLeft;
+        return result;
+    }
+    ~SegmentTree(){
+        for(auto n : nodes){
+            delete n;
+        }
+    }
 };
+
 
 vector<string> split(string str, char divider){
     vector<string> result;
@@ -184,6 +283,35 @@ int main() {
     rootTown->setParentPath(nullptr);
     propagateParent(rootTown);
 
+    vector<Leaf *> tourTownOrder;
+    vector<pair<Connection *, bool>> tourStreetOrder;
+
+    int tmp = 0;
+
+    eulerTourIndexing(rootTown, tmp, tourTownOrder);
+
+    for(int i = 0; i < tourTownOrder.size(); i++){
+        Leaf * node = tourTownOrder[i];
+
+
+        if(node->parentPath != nullptr && node->parentPath->parent == tourTownOrder[i - 1]){
+            tourStreetOrder.push_back(make_pair(node->parentPath, true));
+        }
+        if(i > 0 && tourTownOrder[i - 1]->parentPath != nullptr && tourTownOrder[i - 1]->parentPath->parent == node){
+            tourStreetOrder.push_back(make_pair(tourTownOrder[i - 1]->parentPath, false));
+        }
+
+
+        if(node->firstOccurrenceInEuler == -1){
+            node->firstOccurrenceInEuler = i;
+        }
+        node->lastOccurrenceInEuler = i;
+    }
+
+
+    SegmentTree segmentTree(tourTownOrder);
+
+    cout<<"";
 
     for(int i = 0; i < requestsCount; i ++){
         getline(cin, line);
@@ -196,12 +324,17 @@ int main() {
         else if(request == 'B'){
             Connection * targetStreet = streets.at(stoi(args[1]) - 1);
             int newToy = stoi(args[2]) - 1;
-            
+
         }
     }
 
-    //pair<int,int> t = commonPart(make_pair(0,10), make_pair(3,11));
 
+    for(Leaf * n : towns){
+        delete n;
+    }
+    for(Connection * c : streets){
+        delete c;
+    }
     time(&end);
     cout << "execution time = " << double(end - start);
     return 0;
